c48515e2245205f0c11c8981cf16f754d26390da
sakarin.po <sakarin.po@baac.or.th>
2018-07-09 16:47:14 +0700

    [Edited]พิ่ม CRUD

--- src/main.go
+++ src/main.go
@@ -28,10 +28,21 @@ func main() {
 	}
 	fmt.Println("connect success")
 	defer db.Close() //ทำงานจบจะปิดการทำงานของfunctionทันที
+	//fmt.Println(add(db))
+	// fmt.Println(read(db))
+	//fmt.Println(remove(db, "1"))
+	//fmt.Println(readByCitizentId(db, "3380074028245"))
+	fmt.Println(readByCitizentId2(db, "3380074028245"))
+	//fmt.Println(edit(db, "3", "กก"))
+	//fmt.Println(read(db))
+}
+
+func read(db *sql.DB) []UserData {
+	var userDataList []UserData
 	results, _ := db.Query("SELECT * FROM user")
 	for results.Next() {
 		var userData = UserData{}
-		err = results.Scan(
+		err := results.Scan(
 			&userData.Id,
 			&userData.CitizentId,
 			&userData.Firstname,
@@ -47,6 +58,89 @@ func main() {
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Println(userData)
+		userDataList = append(userDataList, userData)
 	}
+	return userDataList
+}
+
+func add(db *sql.DB) bool {
+	statement, _ := db.Prepare(`INSERT INTO user
+		(citizen_id, firstname,
+		lastname, birthyear, firstname_father,
+		lastname_father, firstname_mother,
+		lastname_mother, soldier_id,address_id)
+		VALUES(?,?,?,?,?,?,?,?,?,?)`) //คำสั่งเก็บในชุดคำสั่งไว้ excute
+	defer statement.Close()
+	_, err := statement.Exec("5573183074474", "สวัสดี", "ประเทศไทย", "1999", "เดินหน้า", "ประเทศไทย", "สบายดี", "ประเทศไทย", "50", "1")
+	if err != nil {
+		panic(err.Error())
+		return false
+	}
+	return true
+}
+
+func remove(db *sql.DB, id string) bool {
+	statement, _ := db.Prepare("DELETE FROM user WHERE user_id=?")
+	defer statement.Close()
+	_, err := statement.Exec(id)
+	if err != nil {
+		panic(err.Error())
+		return false
+	}
+	return true
+}
+
+func edit(db *sql.DB, id, firstnameFather string) bool {
+	statement, _ := db.Prepare("UPDATE user SET firstname_father=? WHERE user_id=?")
+	defer statement.Close()
+	_, err := statement.Exec(firstnameFather, id)
+	if err != nil {
+		panic(err.Error())
+		return false
+	}
+	return true
+}
+
+func readByCitizentId(db *sql.DB, citizentId string) UserData {
+	var userData UserData
+	results, _ := db.Query("SELECT * FROM user WHERE citizen_id=?", citizentId)
+	for results.Next() {
+		err := results.Scan(
+			&userData.Id,
+			&userData.CitizentId,
+			&userData.Firstname,
+			&userData.Lastname,
+			&userData.BirthYear,
+			&userData.FirstnameFather,
+			&userData.LastnameFather,
+			&userData.FirstnameMother,
+			&userData.LastnameMother,
+			&userData.SoldierId,
+			&userData.AddressId,
+		)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	return userData
+}
+
+func readByCitizentId2(db *sql.DB, citizentId string) UserData {
+	var userData UserData
+	err := db.QueryRow("SELECT * FROM user WHERE citizen_id=?", citizentId).Scan(&userData.Id,
+		&userData.CitizentId,
+		&userData.Firstname,
+		&userData.Lastname,
+		&userData.BirthYear,
+		&userData.FirstnameFather,
+		&userData.LastnameFather,
+		&userData.FirstnameMother,
+		&userData.LastnameMother,
+		&userData.SoldierId,
+		&userData.AddressId,
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+	return userData
 }
